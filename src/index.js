d8ba3b69c536c2230497dcf464a64d23a5f2e6c5
Circus-Pygargus <richard.meuret@laposte.net>
2023-03-30 22:58:07 +0200

    [FEAT] : 1st use og io : server logs a message on new connection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const { Server } = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 const port = process.env.port || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('New web socket connection !');
+});
+
+server.listen(port, () => {
     console.log(`Server is up on port 3000`);
 })
